8eddd6ee02d0d69d26cf8d23c75870ae41c743f7
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-01-05 19:44:57 +0900

    ページ内遷移設定

--- js/script.js
+++ js/script.js
@@ -26,6 +26,18 @@ $(function(){
   window.removeEventListener( 'touchmove' , movefun, { passive: false } );
   });
 
+  
+// ページ内スクロール====================================
+$('a[href^="#"]').click(function(event) {
+  // クリックした要素のhref属性の値を取得
+  var anchor = $(this).attr("href"); 
+  // 表示位置を取得
+  var position = $(anchor).offset().top; 
+  // アニメーションの効果の作成
+  $('body,html').animate({ scrollTop : position }, 500); 
+  return false;
+}); 
+
 
 // liveスライドイメージ====================================
   var page=0;
