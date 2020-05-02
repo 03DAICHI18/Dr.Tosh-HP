c99ac80e0f74adf4fc14f02bae96ccf3715c9eb2
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-05-02 21:19:58 +0900

    liveスライドショー横スクロールへ変更、動画読み込み削除＋Spotify読み込み

--- js/script.js
+++ js/script.js
@@ -1,76 +1,47 @@
 // ↓ready関数
-$(function(){
-// ローディンング制御（2）====================================
+$(function () {
+  // ローディンング制御（2）====================================
   var h = $(window).height(); //ブラウザウィンドウの高さを取得
-  $('.load , .load__animation').height(h).css('display','block'); //ウィンドウの高さに合わせでローディング画面を表示
+  $(".load , .load__animation").height(h).css("display", "block"); //ウィンドウの高さに合わせでローディング画面を表示
 
+  // ハンバーガーメニュー====================================
+  $("#drawer-btn , .drawer-menu__hidden").on("click", function () {
+    $("#drawer-btn").toggleClass("open");
 
-// ハンバーガーメニュー====================================
-  $('#drawer-btn , .drawer-menu__hidden').on('click',function(){
-  $('#drawer-btn').toggleClass('open');
+    if ($(this).hasClass("open")) {
+      $("body , #drawer-btn").addClass("open");
+    } else {
+      $("body , #drawer-btn").removeClass("open");
+    }
 
-  if ($(this).hasClass('open')){
-    $('body , #drawer-btn').addClass('open');
-  } else {
-    $('body , #drawer-btn').removeClass('open');
-  }
+    $("#drawer-menu").slideToggle();
 
-  $('#drawer-menu').slideToggle();
-
-  var movefun = function( event ){
-	event.preventDefault();
-  }
-  // スクロール停止の処理
-  window.addEventListener( 'touchmove' , movefun , { passive: false } );
-  // スクロール停止することを停止する処理
-  window.removeEventListener( 'touchmove' , movefun, { passive: false } );
+    var movefun = function (event) {
+      event.preventDefault();
+    };
+    // スクロール停止の処理
+    window.addEventListener("touchmove", movefun, { passive: false });
+    // スクロール停止することを停止する処理
+    window.removeEventListener("touchmove", movefun, { passive: false });
   });
 
-  
-// ページ内スクロール====================================
-$('a[href^="#"]').click(function(event) {
-  // クリックした要素のhref属性の値を取得
-  var anchor = $(this).attr("href"); 
-  // 表示位置を取得
-  var position = $(anchor).offset().top; 
-  // アニメーションの効果の作成
-  $('body,html').animate({ scrollTop : position }, 500); 
-  return false;
-}); 
-
-
-// liveスライドイメージ====================================
-  var page=0;
-  var lastPage =parseInt($("#slideshow img").length-1);
-  $("#slideshow img").css("display","none");
-  $("#slideshow img").eq(page).css("display","block");
-
-  function changePage(){
-    $("#slideshow img").fadeOut(1000);
-    $("#slideshow img").eq(page).fadeIn(1000);
-  };
-
-  var Timer;
-function startTimer(){
-Timer =setInterval(function(){  
-  if(page === lastPage){
-    page = 0;
-    changePage();
-    } else {
-      page ++;
-      changePage();
-        };
-          },1500);
-}
-startTimer();
-  
+  // ページ内スクロール====================================
+  $('a[href^="#"]').click(function (event) {
+    // クリックした要素のhref属性の値を取得
+    var anchor = $(this).attr("href");
+    // 表示位置を取得
+    var position = $(anchor).offset().top;
+    // アニメーションの効果の作成
+    $("body,html").animate({ scrollTop: position }, 500);
+    return false;
+  });
 });
 
 // ↓load関数
 // ローディング制御（1）====================================
-$(window).on('load', function () {
-  $('.load').delay(1000).fadeOut(1000); 
-  $('.load__animation').delay(1000).fadeOut(1000); 
+$(window).on("load", function () {
+  $(".load").delay(1000).fadeOut(1000);
+  $(".load__animation").delay(1000).fadeOut(1000);
   // $('.load , .load__animation').height(h).css('display','hidden');
-  $('#wrap').css('display', 'block'); // ページ読み込みが終わったらメインコンテンツを表示する
+  $("#wrap").css("display", "block"); // ページ読み込みが終わったらメインコンテンツを表示する
 });
