767b4ca2ab1bc2e7f2892568b948e03c34bcb816
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-07-19 11:07:17 +0900

    load削除（HTML・SCSSファイル・js）

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 // ↓ready関数
 $(function () {
-  // ローディンング制御（2）====================================
-  var h = $(window).height(); //ブラウザウィンドウの高さを取得
-  $(".load , .load__animation").height(h).css("display", "block"); //ウィンドウの高さに合わせでローディング画面を表示
-
   // ハンバーガーメニュー====================================
   $("#drawer-btn , .drawer-menu__hidden").on("click", function () {
     $("#drawer-btn").toggleClass("open");
@@ -22,9 +18,13 @@ $(function () {
       event.preventDefault();
     };
     // スクロール停止の処理
-    window.addEventListener("touchmove", movefun, { passive: false });
+    window.addEventListener("touchmove", movefun, {
+      passive: false
+    });
     // スクロール停止することを停止する処理
-    window.removeEventListener("touchmove", movefun, { passive: false });
+    window.removeEventListener("touchmove", movefun, {
+      passive: false
+    });
   });
 
   // ページ内スクロール====================================
@@ -34,16 +34,9 @@ $(function () {
     // 表示位置を取得
     var position = $(anchor).offset().top;
     // アニメーションの効果の作成
-    $("body,html").animate({ scrollTop: position }, 500);
+    $("body,html").animate({
+      scrollTop: position
+    }, 500);
     return false;
   });
 });
-
-// ↓load関数
-// ローディング制御（1）====================================
-$(window).on("load", function () {
-  $(".load").delay(300).fadeOut(1000);
-  $(".load__animation").delay(300).fadeOut(1000);
-  $("#wrap").css("display", "block"); // ページ読み込みが終わったらメインコンテンツを表示する
-});
-
